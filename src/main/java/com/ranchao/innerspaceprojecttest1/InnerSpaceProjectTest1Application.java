952a822f6dcd6af32e594c0dacc325435a6f385d
ranchao <1908220501@qq.com>
2023-04-09 23:47:57 +0800

    初始化项目

---
+++ src/main/java/com/ranchao/innerspaceprojecttest1/InnerSpaceProjectTest1Application.java
@@ -0,0 +1,13 @@
+package com.ranchao.innerspaceprojecttest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerSpaceProjectTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerSpaceProjectTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ranchao/innerspaceprojecttest1/InnerSpaceProjectTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranchao.innerspaceprojecttest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerSpaceProjectTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
