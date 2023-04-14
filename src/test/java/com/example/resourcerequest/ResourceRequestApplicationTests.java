f73adf4f2ed7b3e09a6e997d5d04f4340cce88e1
ranchao <1908220501@qq.com>
2023-04-14 19:43:07 +0800

    7778端口用于处理所有的资源访问

---
+++ src/test/java/com/example/resourcerequest/ResourceRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourcerequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
