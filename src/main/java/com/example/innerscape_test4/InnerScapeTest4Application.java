c8b67151a62199cfee14fe3c041730b55de14540
RainbowBean12138 <130229718+RainbowBean12138@users.noreply.github.com>
2023-04-21 21:34:18 +0800

    Create InnerScapeTest4Application.java

---
+++ src/main/java/com/example/innerscape_test4/InnerScapeTest4Application.java
@@ -0,0 +1,13 @@
+package com.example.innerscape_test4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerScapeTest4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerScapeTest4Application.class, args);
+    }
+
+}
