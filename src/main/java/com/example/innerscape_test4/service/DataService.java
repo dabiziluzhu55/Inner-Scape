a121734fbf5140896eb3c62c18df86577824fc9c
RainbowBean12138 <130229718+RainbowBean12138@users.noreply.github.com>
2023-04-21 21:34:28 +0800

    Create DataService.java

---
+++ src/main/java/com/example/innerscape_test4/service/DataService.java
@@ -0,0 +1,24 @@
+package com.example.innerscape_test4.service;
+
+import com.example.innerscape_test4.entity.Star;
+import com.example.innerscape_test4.entity.StarLittle;
+import com.example.innerscape_test4.entity.StarReply;
+
+import java.util.List;
+
+public interface DataService {
+    public int UserExist(String userID);
+    public int Register(String userID,String userName,int headshot);
+    public String Refresh0(String userID);
+    public int Refresh1(String userID);
+    public List<StarLittle> CatchRefreshStars(String refreshID);
+    public String SelectStars(String userID);
+    public String ReSelectStars(String userID);
+    public int FlyStar(String userID,String starContent);
+    public int ReplyStar(String starID,String userID,String replySay,String refreshID);
+    public List<Star>ViewMyReply(String userID);
+    public List<StarReply>ViewReplyToOthers(String userID);
+    public int DeleteMyReply(String replyID);
+    public int DeleteOtherReplyToMe(String starID,String replyID);
+    public int StarLandfall(String starID,String userID);
+}
