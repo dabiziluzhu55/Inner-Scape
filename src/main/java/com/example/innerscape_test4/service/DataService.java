81f6486dc984ae4b92b7247ff2fa152387fa3b88
RainbowBean12138 <130229718+RainbowBean12138@users.noreply.github.com>
2023-05-06 19:10:38 +0800

    Update DataService.java

--- src/main/java/com/example/innerscape_test4/service/DataService.java
+++ src/main/java/com/example/innerscape_test4/service/DataService.java
@@ -3,12 +3,16 @@ package com.example.innerscape_test4.service;
 import com.example.innerscape_test4.entity.Star;
 import com.example.innerscape_test4.entity.StarLittle;
 import com.example.innerscape_test4.entity.StarReply;
+import com.example.innerscape_test4.entity.User;
 
 import java.util.List;
 
 public interface DataService {
     public int UserExist(String userID);
     public int Register(String userID,String userName,int headshot);
+    public List<User> Login(String userID);
+    public int ChangeHeadshot(String userID,int headshot);
+    public int ChangeName(String userID,String userName);
     public String Refresh0(String userID);
     public int Refresh1(String userID);
     public List<StarLittle> CatchRefreshStars(String refreshID);
@@ -21,4 +25,5 @@ public interface DataService {
     public int DeleteMyReply(String replyID);
     public int DeleteOtherReplyToMe(String starID,String replyID);
     public int StarLandfall(String starID,String userID);
+    public int GetNewInfo(String userID);
 }
